f8d1d5bdfa6640bc0d6c51151dd0efd19101c4fd
Caleb-Stripes <calebdstripes@gmail.com>
2020-02-13 23:29:47 -0500

    here is hoping for a working project

---
+++ src/main/java/org/wecancodeit/Route/Review/Site/RouteReviewSiteApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.Route.Review.Site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteReviewSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteReviewSiteApplication.class, args);
+	}
+
+}
